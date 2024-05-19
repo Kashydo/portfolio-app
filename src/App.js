79f1d394603a27955fe119aa5803cf96a05fa0f6
Kashydo <kaszydo@gmail.com>
2024-05-19 15:01:42 +0200

    create grid

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div class="wrapper">
+        <div class="grid1">1</div>
+        <div class="grid2">2</div>
+        <div class="grid3">3</div>
+        <div class="grid4">4</div>
+        <div class="grid5">5</div>
+        <div class="grid6">6</div>
+        <div class="grid7">7</div>
+        <div class="grid8">8</div>
         
-        <p>
-          Portfolio App
-         
-        </p>
-      </header>
+      </div>
     </div>
   );
 }
