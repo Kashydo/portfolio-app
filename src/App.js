c009ec39b45a63562be6c8d258c195fbee74a4f6
Kashydo <kaszydo@gmail.com>
2024-07-22 16:05:29 +0200

    feat: Welcome message Welcome message is shown using typewritter effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import AboutMe from './AboutMe.js'
+import Typewriter from './Typewriter.js';
 
 function App() {
   return (
     <div className="App">
       <div class="wrapper">
         <div class="gridAboutMe"><AboutMe/></div>
-        <div class="gridWelcome">2</div>
+        <div class="gridWelcome"><h2><Typewriter text="Welcome to my portfolio" delay={100}/></h2> </div>
         <div class="gridProjects">3</div>
         <div class="gridContact">4</div>
         <div class="gridBlog">5</div>
